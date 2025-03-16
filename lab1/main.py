3742d5b78e4d752165d3bb4237a92b34c41e7319
catchtimings <chindina.63@gmail.com>
2025-03-16 10:03:37 +0400

    [+] using a settings.json with directories

--- lab1/main.py
+++ lab1/main.py
@@ -1,22 +1,25 @@
-from file_work import read_from_file, read_data, write_to_file, save_data
+from file_work import read_json, read_txt, save_json, save_txt
 from task1.text_encryption import text_encryption
 from task2.frequency_analysis import frequency_count, sort_dict
 
 
 def main():
     try:
+        TASK1_FILES = read_json("settings.json").get("task1")
+        TASK2_FILES = read_json("settings.json").get("task2")
+
         # task1
-        text = read_from_file("task1/text.txt")
-        key = read_data("task1/key.json")
+        text = read_txt(TASK1_FILES["plain_text"])
+        key = read_json(TASK1_FILES["key"])
         encrypted_text = text_encryption(text, key)
-        write_to_file("task1/encrypted_text.txt", encrypted_text)
+        save_txt(TASK1_FILES["encrypted_text"], encrypted_text)
 
         # task2
-        cod25 = read_from_file("task2/cod25.txt")
-        save_data("task2/frequency.json", sort_dict(frequency_count(cod25)))
-        key_cod25 = read_data("task2/key.json")
+        cod25 = read_txt(TASK2_FILES["plain_text"])
+        save_json(TASK2_FILES["frequency"], sort_dict(frequency_count(cod25)))
+        key_cod25 = read_json(TASK2_FILES["key"])
         decrypted_text = text_encryption(cod25, key_cod25)
-        write_to_file("task2/decrypted_text.txt", decrypted_text)
+        save_txt(TASK2_FILES["decrypted_text"], decrypted_text)
     except Exception as e:
         print(f"Something went wrong: {e}")
 
