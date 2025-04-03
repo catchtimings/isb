eebf81789e32347661d50164b201b9d4e1358cd2
catchtimings <chindina.63@gmail.com>
2025-04-03 17:32:45 +0400

    [+] parse arguments

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,22 @@
+import argparse
+from file_work import read_data
+
+
+def get_arguments() -> str:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_file", type=str, help="directory to json file")
+    return parser.parse_args().json_file
+
+
 def main():
-    print("Test")
+    try:
+        config = read_data(get_arguments())
+
+        print(read_data(config["cpp_seq"]))
+        print(read_data(config["java_seq"]))
+    except Exception as e:
+        print(e)
+
 
 if __name__ == "__main__":
     main()
