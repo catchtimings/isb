dc0243d8293128d55588cbbb66549bb7c6f58272
catchtimings <chindina.63@gmail.com>
2025-04-03 19:16:11 +0400

    [+] test for equally consecutive bits

--- lab2/task2/tests.py
+++ lab2/task2/tests.py
@@ -1,7 +1,26 @@
-from math import erfc, sqrt
-
+import math
 
 def frequency_bit_test(data: str) -> float:
-    s_n = (1 / sqrt(len(data))) * (data.count("1") - data.count("0"))
-    p_value = erfc((s_n) / sqrt(2))
+    s_n = (data.count("1") - data.count("0")) / math.sqrt(len(data))
+    p_value = math.erfc(abs(s_n) / math.sqrt(2))
+    return p_value
+
+
+def equally_consecutive_bits(data: str) -> float:
+    seq_len = len(data)
+    zeta = data.count("1") / seq_len
+
+    if not (abs(zeta - 0.5) < (2 / math.sqrt(seq_len))):
+        return 0
+
+    v_n = 0
+    for i in range(seq_len - 1):
+        if data[i] == data[i + 1]:
+            v_n += 0
+        else:
+            v_n += 1
+
+    numerator = v_n - 2 * seq_len * (1 - zeta)
+    denominator = 2 * math.sqrt(2 * seq_len) * zeta * (1 - zeta)
+    p_value = math.erfc(abs(numerator) / denominator)
     return p_value
