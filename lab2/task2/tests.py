3138d7581a58a0a80f1004f8f5b5683ff8e24dee
catchtimings <chindina.63@gmail.com>
2025-04-04 12:08:41 +0400

    [+] longest_sequence_test function

--- lab2/task2/tests.py
+++ lab2/task2/tests.py
@@ -1,5 +1,8 @@
 import math
 
+from spicy import special
+
+
 def frequency_bit_test(data: str) -> float:
     """
     The function checks if sequence is random
@@ -38,4 +41,36 @@ def equally_consecutive_bits(data: str) -> float:
     return p_value
 
 
-def longest_sequence_test(data: str)
+def longest_sequence_test(data: str) -> float:
+    """
+    The function checks if sequence is random
+    using test for the longest sequence in block
+    :param data: sequence
+    :return: result
+    """
+    blocks = [data[i : i + 8] for i in range(0, len(data), 8)]
+    v = [0, 0, 0, 0]
+
+    for block in blocks:
+        mx_length = float("-inf")
+        curr_length = 0
+        for digit in block:
+            if digit == "1":
+                curr_length += 1
+                mx_length = max(mx_length, curr_length)
+            else:
+                curr_length = 0
+        match mx_length:
+            case mx_length if mx_length <= 1:
+                v[0] += 1
+            case 2:
+                v[1] += 1
+            case 3:
+                v[2] += 1
+            case mx_length if mx_length >= 4:
+                v[3] += 1
+
+    p = [0.2148, 0.3672, 0.2305, 0.1875]
+    xi_square = sum((v[i] - 16 * p[i]) ** 2 / (16 * p[i]) for i in range(len(v)))
+    p_value = special.gammainc((3 / 2), (xi_square / 2))
+    return p_value
